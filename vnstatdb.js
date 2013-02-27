c502592ef7dc3f27b706b36ed079ce58013d36c6
TheRaven <ysimard@gmail.com>
2013-02-26 21:05:28 -0500

    added development configuration

--- vnstatdb.js
+++ vnstatdb.js
@@ -1,9 +1,15 @@
+var CONFIG = require('config').vnstatdb;
 var express = require('express');
  
 var app = express();
  
-app.get('/summary', function(req, res) {
+
+function summary(req, res) {
     res.send({"hour":{"rx":"8547","tx":"3109"},"day":{"rx":588935,"tx":42222},"month":{"rx":4226515,"tx":670194},"total":{"act":1,"rx":4226515,"tx":670194}});
-});
+}
+
+
+app.get('/', summary);
+app.get('/summary', summary);
  
-app.listen(3000);
+app.listen(CONFIG.http_port);
