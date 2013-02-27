32b7608152b323533af91a8b8c3aed1f4cff1f33
TheRaven <ysimard@gmail.com>
2013-02-27 16:55:41 -0500

    Render summary data for current hour, day, month and total as well as interface name and nickname

---
+++ routes/summary.js
@@ -0,0 +1,59 @@
+var CONFIG = require('config').vnstatdb;
+var fs = require('fs');
+
+/*
+	find lines number in the file and returns them
+*/
+function getLinesInDatabaseFile(filename, lineNumbers, callback) {
+	
+	var fileLines = [];
+	var data = fs.readFileSync(filename, 'utf8');
+    var lines = data.split("\n");
+    for(var i=0; i<lines.length; i++) {
+		if(lineNumbers.indexOf(i) >= 0) {
+			fileLines.push(lines[i])
+		}
+    }
+	
+    callback(null, fileLines);
+}
+
+function parseLine(line) {
+	var data = line.split(";");
+	
+	var rx = data[3];
+	if(data[0] != "h") {
+		rx = rx * 1024 + parseInt(data[5]);
+	}
+	
+	var tx = data[4];
+	if(data[0] != "h") {
+		tx = tx * 1024 + parseInt(data[6]);
+	}
+	
+	return {
+		"rx": rx,
+		"tx": tx
+	}
+	
+}
+
+
+exports.summary = function(req, res) {
+	getLinesInDatabaseFile(CONFIG.database_file_location, [2,3,13,43,55,65], function( err, lines) {
+		var summary = {
+			"interface": lines[0].split(";")[1],
+			"nickname": lines[1].split(";")[1],
+		}
+		summary.hour = parseLine(lines[5]);
+		summary.day = parseLine(lines[2]);
+		summary.month = parseLine(lines[3]);
+		summary.total = parseLine(lines[4]);
+		res.send(summary)
+
+
+	})
+
+}
+
+
--- vnstatdb.js
+++ vnstatdb.js
@@ -1,15 +1,14 @@
 var CONFIG = require('config').vnstatdb;
 var express = require('express');
- 
+
+var routes = require('./routes/summary'); 
 var app = express();
- 
-
-function summary(req, res) {
-    res.send({"hour":{"rx":"8547","tx":"3109"},"day":{"rx":588935,"tx":42222},"month":{"rx":4226515,"tx":670194},"total":{"act":1,"rx":4226515,"tx":670194}});
-}
 
 
-app.get('/', summary);
-app.get('/summary', summary);
+
+
+
+app.get('/', routes.summary);
+app.get('/summary', routes.summary);
  
 app.listen(CONFIG.http_port);
